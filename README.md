164f992e320fdf16609caf2e6ee150bb97521961
zweed4u <zackweeden@aol.com>
2015-07-12 21:41:31 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a script that pushes all files in a local directory to an already created, remote repo. I'm still learning the ropes of git and github so this is experimental. This script must be located inside the directory of the desired local repo. It is self checking so that this git script isn't pushed along with the rest of the files in the folder but it is unstable. Whitespace in commit names seem to be cause it to be temperamental. One word named commits seem to be fine. Underscore naming works as well. 
+
+Contact me with any questions: @ZWeed4U
---
+++ git.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+bold=$(tput bold)
+normal=$(tput sgr0)
+directory=$(pwd)"/*" 
+adjust=$(echo $directory | sed 's/ \+/\_/g')
+
+
+echo -e "Make sure this shell file is in the local project's directory"
+
+echo -e "${bold}Username${normal}?"
+read user
+
+
+echo -e '"Github email? (author purposes)"'
+read email
+
+
+git config --global user.name $user
+git config --global user.email $email
+
+
+git init
+
+echo -e "local repo created"
+
+sleep .5
+
+
+
+
+for file in .* *
+do 
+	if [[ $file == "git.sh" ]]; then
+		: #Don't add THIS script to repo	
+	elif [[ $file == ".git" ]]; then
+		: #Don't add the init folder
+	else
+		echo $file 'added...'
+		git add "$file"
+	fi
+done
+
+
+
+
+echo -e 'Repo Name (Must match)'
+read repo_Name
+
+echo Commit name?
+read commit_Name
+
+
+git rm --cached git.sh
+
+git commit -m $commit_Name
+
+
+
+
+
+echo -e "Setting remote..."
+git remote add origin https://github.com/$user/$repo_Name.git
+
+echo -e "${bold}Done${normal}"
+
+echo -e "Pushing changes to origin..."
+git push -u origin master
+
+echo -e 'Complete. Check repo for changes.'
